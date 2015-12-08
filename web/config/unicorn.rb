d499478a93634a76e4b23e324d43aa0ca092a4f7
root <root@ircsems.chartoin.com>
2015-12-07 20:50:14 -0500

    Added nginx.conf and unicorn.rb

--- web/config/unicorn.rb
+++ web/config/unicorn.rb
@@ -1,26 +1,18 @@
 # Set the working application directory
-# working_directory "/path/to/your/app"
-
-app_root "/ircsems/experiments/unicornexp-deploy"
-
+app_root "/ircsems/deployment/web"
 working_directory app_root
 
 # Unicorn PID file location
-# pid "/path/to/pids/unicorn.pid"
 pid "{app_root}/tmp/pids/unicorn.pid"
 
 # Path to logs
-# stderr_path "/path/to/log/unicorn.log"
-# stdout_path "/path/to/log/unicorn.log"
 stderr_path "{app_root}/log/unicorn.log"
 stdout_path "{app_root}/log/unicorn.log"
 
 # Unicorn socket
-#listen "/tmp/unicorn.[app name].sock"
-listen "/tmp/unicorn.ir.sock"
+listen "/tmp/unicorn.web.ircsems.sock"
 
 # Number of processes
-# worker_processes 4
 worker_processes 2
 
 # Time-out
