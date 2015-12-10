395834def0aa6d2f0264a85672d98aa25f7d5b53
chartoin <chartoin@gmail.com>
2015-12-10 16:46:22 -0500

    Added scripts to download and normalize tweets

---
+++ scripts/scripts/pyscrape/config.py
@@ -0,0 +1,59 @@
+twitter = {
+    'accounts': [
+        {
+            'handle': 'charuvah',
+            'consumer': {
+                'key': '8Of9gohFxP466i47x0N1pVRlr',
+                'secret': 'fPnHIX3x8RFfsozLpLEaEAlacRhVxnGksXJEHHoZyePLgJdGu1'
+            },
+            'oauth': {
+                'token': '3561450377-gMe4xOOj8u9kLg1KKk1viC7XS9ekSWxWDKoOske',
+                'secret': 'f4mYcERUKqAjKvpLXQjEtw5M4ErwLD6A37ojqKil4yEOp'
+            },
+            'lists': {
+                'en': [ 'CNN' ],
+                'de': [ 'MEEDIA' ],
+                'ru': [ 'vesti_news' ],
+                'fr': [ 'allocine' ]
+            }
+        },
+        {
+            'handle': 'nullset14',
+            'consumer': {
+                'key': 'jvJCyZrf8pfnZiQeYg60Dmr4i',
+                'secret': 'lTUfVnK9Q9b8MAz4tQ6eLonTVM72XdO1QAx6lvxr3pRPQhvIGi'
+            },
+            'oauth': {
+                'token': '3545600543-DHLGHNBPon6mFXwl28WkqkPZN6B1kTmiokB09Do',
+                'secret': 'YO2kMRf8uJMdGpyR9tt03548HsSyHgYrbDy5e6dUw9Ies'
+            },
+            'lists': {
+                'en': [ 'BBCWorld' ],
+                'de': [ 'SZ' ],
+                'ru': [ 'RT_russian' ],
+                'fr': [ 'Le_Figaro' ]
+            }
+        },
+        {
+            'handle': 'bizentass',
+            'consumer': {
+                'key': 'YaQo4SYY3I8gHwqtV7NBFWhfI',
+                'secret': 'WXzP1bH4R1OmTdfPosfxE63DvAcX6qYe3ykOUnybLSa7MgB7Ww'
+            },
+            'oauth': {
+                'token': '50938959-C8GGxEox3YxUAghLmkazZWm8PljlaCupwCBGeVdSa',
+                'secret': 's9wX0SitxZBGRhKMQYpcRcR224cDv1jPWn1GeoYDWDQGW'
+            },
+            'lists': {
+                'en': [ 'nytimes', 'USATODAY' ],
+                'de': [ 'BVB', 'svouseman', 'bundesliga_de' ],
+                'ru': [ 'sportsru', 'Sovsport_hockey' ],
+                'fr': [ 'itele', 'afpfr' ]
+            }
+        }
+    ],
+    'languages': [ 'en', 'de', 'ru', 'fr' ],
+    'download_dir': '/data/edu/school/ub/cse/courses/sem-1/535/proj/c/src/scripts/data/downloaded',
+    'normalization_dir': '/data/edu/school/ub/cse/courses/sem-1/535/proj/c/src/scripts/data/normalized',
+    'normalized_filename': 'tweets.json'
+}
